c90e27886b0c378c68b793378dd6f758351f3e8c
Jason <jazzmoret@gmail.com>
2023-10-23 14:25:07 +0200

    débogage middleware auth

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -2,7 +2,12 @@ import jwt, { JwtPayload } from "jsonwebtoken";
 import { Response, NextFunction } from "express";
 import { AuthenticatedRequest } from "./types.js";
 import "dotenv/config";
-import { HTTP_FORBIDDEN, HTTP_WRONG_CREDENTIALS } from "./status.js";
+import {
+  HTTP_BAD_REQUEST,
+  HTTP_FORBIDDEN,
+  HTTP_SERVER_ERROR,
+  HTTP_WRONG_CREDENTIALS,
+} from "./status.js";
 
 export const auth = async (
   req: AuthenticatedRequest,
@@ -12,21 +17,19 @@ export const auth = async (
   try {
     const jwtToken = req.header("token");
     if (!jwtToken) {
-      return res.status(HTTP_FORBIDDEN).send("Not Authorized");
+      return res.status(HTTP_BAD_REQUEST).send("Token required");
     }
-    const payload: JwtPayload = jwt.verify(
-      jwtToken,
-      process.env.jwtSecret!
-    ) as JwtPayload;
-    if (!payload) {
-      return res.status(HTTP_WRONG_CREDENTIALS).send("Not Authorized");
-    }
-    req.user = payload.userId;
-    req.role = payload.role;
-    req.mail = payload.mail;
-    next();
+    jwt.verify(jwtToken, process.env.jwtSecret!, async function (err, payload) {
+      if (err) {
+        return res.status(HTTP_FORBIDDEN).json({ message: "Invalid token" });
+      }
+      req.user = (payload as JwtPayload).userId;
+      req.role = (payload as JwtPayload).role;
+      req.mail = (payload as JwtPayload).mail;
+      next();
+    });
   } catch (err: any) {
     console.error(err.message);
-    return res.status(HTTP_FORBIDDEN).send("Not Authorized");
+    return res.status(HTTP_SERVER_ERROR).send("Error server");
   }
 };
